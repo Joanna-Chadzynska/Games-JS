4c4f58d8ddf15390a0619e481a749481313b507f
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-06 20:29:27 +0200

    start game

--- Hangman/src/app/app.ts
+++ Hangman/src/app/app.ts
@@ -1,1 +1,13 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import './vendor';
+
+import { Game } from './components/game';
+
+const newGame = new Game({
+  lettersWrapper: document.getElementById('letters')! as HTMLDivElement,
+  categoryWrapper: document.getElementById('category')! as HTMLDivElement,
+  wordWrapper: document.getElementById('word')! as HTMLDivElement,
+  outputWrapper: document.getElementById('output')! as HTMLDivElement
+});
+
+newGame.start();
