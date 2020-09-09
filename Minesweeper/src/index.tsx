72c2645c0427089dad5f611bb3c11de4e2c4034e
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-09 18:21:06 +0200

    setup app with cra

--- Minesweeper/src/index.tsx
+++ Minesweeper/src/index.tsx
@@ -1,21 +1,21 @@
-import * as React from 'react';
-import { render } from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
 
-import './assets/styles/main.scss';
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-type App = {
-	hi?: string;
-};
-
-const App: React.SFC<App> = ({ hi }) => {
-	return (
-		<div className='container'>
-			<h1>hello</h1>
-			<p>{hi}</p>
-		</div>
-	);
-};
-
-export default App;
-
-render(<App />, document.getElementById('app'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
