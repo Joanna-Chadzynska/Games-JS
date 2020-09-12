c577c6560b199093e0208d0f5f8afe73ec4500f8
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-12 20:40:51 +0200

    add game options add config

--- Minesweeper/src/App.tsx
+++ Minesweeper/src/App.tsx
@@ -1,26 +1,52 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState } from 'react';
-
+import { useSelector } from 'react-redux';
 import './styles/app.scss';
 
 import Theme from './styles/themes/theme';
 import { Main } from './styles/App.styles';
 import { GlobalStyle } from './styles/globalStyles';
 import { Header, Board, Panel } from './features';
+import { RootState } from './app/store';
+
+export type GameOptions = {
+	easy: {
+		cols: number;
+		rows: number;
+		mines: number;
+	};
+	normal: {
+		cols: number;
+		rows: number;
+		mines: number;
+	};
+	expert: {
+		cols: number;
+		rows: number;
+		mines: number;
+	};
+	custom: {
+		cols: number;
+		rows: number;
+		mines: number;
+	};
+};
 
 const App = () => {
-	const [config, setConfig] = useState({
-		cols: 9,
-		rows: 5,
-		mines: 10,
-	});
+	const game = useSelector((state: RootState) => state.game);
+	const config: GameOptions = {
+		easy: { cols: 9, rows: 9, mines: 10 },
+		normal: { cols: 16, rows: 16, mines: 40 },
+		expert: { cols: 30, rows: 16, mines: 99 },
+		custom: { cols: 9, rows: 5, mines: 10 },
+	};
 	return (
 		<Theme>
 			<GlobalStyle />
-			<Main cellsInRow={config.cols}>
-				<Header />
-				<Board cols={config.cols} rows={config.rows} mines={config.mines} />
-				<Panel />
+			<Main cellsInRow={game.cols}>
+				<Header mines={game.mines} />
+				<Board config={config} />
+				<Panel config={config} />
 			</Main>
 		</Theme>
 	);
