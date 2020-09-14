dfa4f690d4d303b1c6dc882819699a1defd547a5
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-14 18:33:46 +0200

    create new utils methods

--- Minesweeper/src/features/Game/utils.ts
+++ Minesweeper/src/features/Game/utils.ts
@@ -20,3 +20,21 @@ export const create2DArray = (rows: number, cols: number) => {
 
 	return arr;
 };
+
+export const countMaxMines = (row: number, col: number) => {
+	let width = col - 1;
+	let height = row - 1;
+
+	return width * height;
+};
+
+export const getRandomNumber = (dimension: number) =>
+	Math.floor(Math.random() * 1000 + 1) % dimension;
+
+export const setBubblePosition = (value: number, min: number, max: number) => {
+	const minVal = min ? min : 0;
+	const maxVal = max ? max : 100;
+	const newVal = Number(((value - minVal) * 100) / (maxVal - minVal));
+
+	return newVal;
+};
